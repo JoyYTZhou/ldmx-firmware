54b249094fe1d083a06a46a4f5272cd54de186fe
Benjamin Reese <bareese@slac.stanford.edu>
2024-10-11 09:46:32 -0700

    Fix addressing

--- firmware/common/tdaq/python/ldmx_tdaq/_Lcls2TimingRx.py
+++ firmware/common/tdaq/python/ldmx_tdaq/_Lcls2TimingRx.py
@@ -15,7 +15,7 @@ class Lcls2TimingRx(pr.Device):
 
         self.add(LclsTimingCore.TPGMiniCore(
             name   = "TPGMini",
-            offset = 0x3000))
+            offset = 0x30000))
 
         self.add(ldmx_tdaq.TimingGtCoreWrapper(
             name   = "GTY",
