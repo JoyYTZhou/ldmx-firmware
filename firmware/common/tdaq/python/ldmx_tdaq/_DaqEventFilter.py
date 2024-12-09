02c793f41e502b45fdc05418d0ee674fb7d6d467
Benjamin Reese <bareese@slac.stanford.edu>
2024-12-09 11:48:56 -0800

    Fix DaqEventFilter

--- firmware/common/tdaq/python/ldmx_tdaq/_DaqEventFilter.py
+++ firmware/common/tdaq/python/ldmx_tdaq/_DaqEventFilter.py
@@ -11,25 +11,28 @@ class DaqEventFilter(ris.Slave, ris.Master):
         self.subsytemId = subsytemId
         self.contributorId = contributorId
 
-        self.header = ldmx_tdaq.EventHeader(
-            version=0,
-            subsystemId=0,
-            contributorId=0,
-            bunchCount=0,
-            pulseId=0)
-
     def _acceptFrame(self, frame):
+#        with frame.lock():
         rawNumpy = frame.getNumpy(0, frame.getPayload())
-        self.header.fill_from_numpy(rawNumpy)
+        header = rawNumpy[0:16].view(ldmx_tdaq.EventHeaderDType)
 
         forward = True
-        if (self.subsytemId is not None and self.header.subsytemId != self.subsytemId):
+        if (self.subsytemId is not None and header['subsytemId'] != self.subsytemId):
             forward = False
 
-        if (self.contributorId is not None and self.header.contributorId != self.contributorId):
+        if (self.contributorId is not None and header['contributorId'] != self.contributorId):
             forward = False
+
+        print(f'Got Frame - {header} - {forward=} - {self.__class__.__name__}')                
 
         if forward is True:
             self._sendFrame(frame)
-
+            
         
+class DaqDebug(ris.Slave):
+
+    def _acceptFrame(self, frame):
+        npy = frame.getNumpy()
+
+        header = npy[0:16].view(ldmx_tdaq.EventHeaderDType)
+        print(f'Got frame with header \n {header} \n size = {len(npy[16:])}')
