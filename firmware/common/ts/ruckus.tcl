8c1a0c66607e2040ff4a888b7747b732b42b1098
Benjamin Reese <bareese@slac.stanford.edu>
2025-01-06 13:24:40 -0800

    Load HLS again

--- firmware/common/ts/ruckus.tcl
+++ firmware/common/ts/ruckus.tcl
@@ -5,7 +5,7 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 loadSource -lib ldmx_ts -dir "$::DIR_PATH/rtl/"
 # loadSource -lib ldmx_ts -sim_only -dir  "$::DIR_PATH/sim/"
 
-#loadRuckusTcl "$::DIR_PATH/hls/ts_s30xl_threshold_trigger"
+loadRuckusTcl "$::DIR_PATH/hls/ts_s30xl_threshold_trigger"
 
 if { [info exists ::env(LDMX_TS_XCI)] == 1 && $::env(LDMX_TS_XCI) == 1 } {
     loadIpCore -path "$::DIR_PATH/ip/TsGtyIpCore/TsGtyIpCore.xci"
